1083a802ac767aac4903c1531f85a3d8f737d52f
zuxriddinCsharp <137428899+zuxriddinCsharp@users.noreply.github.com>
2023-10-16 21:35:57 +0500

    Zuxriddin Lesson-01

--- Lesson01/Lesson01/Program.cs
+++ Lesson01/Lesson01/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Lesson01.Models.CoinDesk;
+using Lesson01.Models.RandomUser;
+using Lesson01.Universities_List;
+using Lesson01.Zippopotam;
 using Newtonsoft.Json;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Lesson01
@@ -30,23 +34,125 @@ namespace Lesson01
 
             #region CoinDesk
 
+            //var client = new HttpClient();
+
+            //var webRequest = new HttpRequestMessage(HttpMethod.Get, "https://randomuser.me/api/")
+            //{
+            //    Content = new StringContent("", Encoding.UTF8, "application/json")
+            //};
+
+            //var response = client.Send(webRequest);
+
+            //using var reader = new StreamReader(response.Content.ReadAsStream());
+            //string json = reader.ReadToEnd();
+
+            //CoinDesk result = JsonConvert.DeserializeObject<RandomUser>(json);
+
+
+            //Console.WriteLine($"Last time Bitcoin was updated: {result}");
+
+            #endregion
+
+            #region RandomUser
+
+            //var client = new HttpClient();
+            //try
+            //{
+            //    var webRequest = new HttpRequestMessage(HttpMethod.Get, "https://randomuser.me/api/")
+            //    {
+            //        Content = new StringContent("", Encoding.UTF8, "application/json")
+            //    };
+
+            //    var response = client.Send(webRequest);
+
+            //    using var reader = new StreamReader(response.Content.ReadAsStream());
+            //    string json = reader.ReadToEnd();
+
+            //    RandomUser result = JsonConvert.DeserializeObject<RandomUser>(json);
+
+            //    foreach (var item in result.results)
+            //    {
+            //        Console.WriteLine($"Title:     {item.name.title}");
+            //        Console.WriteLine($"First:     {item.name.first}");
+            //        Console.WriteLine($"Last:      {item.name.last}");
+            //        Console.WriteLine($"Street:    Number - {item.location.street.number}, Name - {item.location.street.name}");
+            //        Console.WriteLine($"City:      {item.location.city}");
+            //        Console.WriteLine($"State:     {item.location.state}");
+            //        Console.WriteLine($"Country:   {item.location.country}");
+            //        Console.WriteLine($"Pastcode:  {item.location.postcode}");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            #endregion
+
+            #region Zippopotam
+            //var client = new HttpClient();
+            //try
+            //{
+            //    var wepRequest = new HttpRequestMessage(HttpMethod.Get, "https://api.zippopotam.us/us/33162")
+            //    {
+            //        Content = new StringContent("", Encoding.UTF8, "application/json")
+            //    };
+            //    var response = client.Send(wepRequest);
+            //    using var reader = new StreamReader(response.Content.ReadAsStream());
+            //    string json = reader.ReadToEnd();
+
+            //    zippopotame result = JsonConvert.DeserializeObject<zippopotame>(json);
+
+            //    Console.WriteLine($"Post code:              {result.postcode}");
+            //    Console.WriteLine($"Country:                {result.country}");
+            //    Console.WriteLine($"Country abbreviation:   {result.countryabbreviation}");
+
+            //    foreach (var item in result.places)
+            //    {
+            //        Console.WriteLine($"Place name:             {item.placename}");
+            //        Console.WriteLine($"Longitude:              {item.longitude}");
+            //        Console.WriteLine($"State:                  {item.state}");
+            //        Console.WriteLine($"State abbreviation:     {item.stateabbreviation}");
+            //        Console.WriteLine($"Latitude:               {item.latitude}");
+
+            //    }
+            //}
+            //catch ( Exception ex )
+            //{
+            //    Console.WriteLine(ex.Message );
+            //}
+            #endregion
+
+            #region Universities List
+            Console.Write("Enter counrty name: ");
+            string country = Console.ReadLine();
+
             var client = new HttpClient();
 
-            var webRequest = new HttpRequestMessage(HttpMethod.Get, "https://api.coindesk.com/v1/bpi/currentprice.json")
+            var wepRequest = new HttpRequestMessage(HttpMethod.Get, $" http://universities.hipolabs.com/search?country={country}")
             {
                 Content = new StringContent("", Encoding.UTF8, "application/json")
             };
 
-            var response = client.Send(webRequest);
+            var respons = client.Send(wepRequest);
 
-            using var reader = new StreamReader(response.Content.ReadAsStream());
+            using var reader = new StreamReader(respons.Content.ReadAsStream());
             string json = reader.ReadToEnd();
 
-            CoinDesk result = JsonConvert.DeserializeObject<CoinDesk>(json);
+            List<Root> universities=JsonConvert.DeserializeObject<List<Root>>(json);
 
+            Console.WriteLine($"Universities in the country: {country}");
 
-            Console.WriteLine($"Last time Bitcoin was updated: {result.time.updatedISO}");
+            foreach (var university in universities)
+            {
 
+                Console.WriteLine();               
+                Console.WriteLine($"Name:      {university.name}");
+                Console.WriteLine($"Country:   {university.country}");
+                Console.WriteLine($"Web Pages: {string.Join(", ", university.web_pages)}");
+                Console.WriteLine();
+                Console.WriteLine("________________________________________________________");
+
+            }
             #endregion
         }
     }
---
+++ Lesson01/Lesson01/Universities List/Universities List.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01.Universities_List
+{
+    public class Root
+    {
+        [JsonProperty("state-province")]
+        public string stateprovince { get; set; }
+        public string country { get; set; }
+        public List<string> domains { get; set; }
+        public List<string> web_pages { get; set; }
+        public string alpha_two_code { get; set; }
+        public string name { get; set; }
+    }
+}
