a3bf2ca3ad976037f3973764a3862203ab864402
Shohboz Xurramov <shohboztuit@gmail.com>
2023-10-19 10:58:41 +0500

    shohboz

--- Lesson01/Lesson01/Models/CatFacts/FactModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lesson01.Models.CatFacts
-{
-    internal class FactModel
-    {
-        public string fact { get; set; }
-        public int length { get; set; }
-    }
-}
---
+++ Lesson01/Lesson01/Models/Unversity/Unversity.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01.Models.Unversity
+{
+    internal class Unversity
+    {
+        [JsonProperty("state-province")]
+        public string stateprovince { get; set; }
+        public string country { get; set; }
+        public List<string> domains { get; set; }
+        public List<string> web_pages { get; set; }
+        public string alpha_two_code { get; set; }
+        public string name { get; set; }
+    }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+   
+
+
+}
