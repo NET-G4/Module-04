b081738e0bffcaea459168270b849b29d86fbe2c
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2023-10-28 17:18:23 +0200

    lesson exercises

--- Lesson06/Lesson06/MainWindow.xaml.cs
+++ Lesson06/Lesson06/MainWindow.xaml.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Lesson06.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +26,255 @@ namespace Lesson06
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly HttpClient _client;
+        private const string UNIVERSITIES_API_URL = "http://universities.hipolabs.com/search?";
+        private static string BITCOIN_API_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+        private Stopwatch stopwatch = new Stopwatch();
+        private List<CoinDesk> bitcoins = new List<CoinDesk>();
+        private CancellationToken token = new CancellationToken();
+
         public MainWindow()
         {
             InitializeComponent();
+            Thread.CurrentThread.Name = "UI Thread";
+
+            _client = new HttpClient();
+        }
+
+        private async void Search_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BeforeLoadingStockData();
+
+                var country = CountryNameInput.Text;
+
+                var source = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+                var result = await GetData(country, source);
+
+                UniversitiesDataGrid.ItemsSource = null;
+                UniversitiesDataGrid.ItemsSource = result;
+
+                // await WriteData(result);
+            }
+            catch (Exception ex)
+            {
+                Notes.Text = ex.Message;
+            }
+            finally
+            {
+                AfterLoadingStockData();
+            }
+        }
+
+        private async Task WriteData(List<University> universities)
+        {
+            var path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            var fullPath = path + $"\\files\\data-{DateTime.Now}.json";
+
+            using (FileStream fs = new FileStream(fullPath, FileMode.OpenOrCreate))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    var json = JsonConvert.SerializeObject(universities);
+                    await sw.WriteAsync(json);
+                }
+            }
+        }
+
+        private async Task<List<University>> GetData(string country, CancellationTokenSource token)
+        {
+            var result = await LoadUniversities(country, token);
+            return result;
+        }
+
+        private void Bitcoin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ThreadPool.QueueUserWorkItem((state) =>
+                {
+                    var coin = GetBitcoin();
+                    bitcoins.Add(coin);
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        Coins.ItemsSource = null;
+                        Coins.ItemsSource = bitcoins;
+                    });
+                });
+            }
+            catch (Exception ex)
+            {
+                Notes.Text = ex.Message;
+            }
+            finally
+            {
+                AfterLoadingStockData();
+            }
+        }
+
+        private async Task<List<University>> LoadUniversities(string country, CancellationTokenSource token)
+        {
+            string url;
+            if (string.IsNullOrEmpty(country))
+            {
+                url = UNIVERSITIES_API_URL;
+            }
+            else
+            {
+                url = UNIVERSITIES_API_URL + "country=" + country;
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Get, url)
+            {
+                Content = new StringContent("", Encoding.UTF8, "application/json")
+            };
+
+            var response = await _client.SendAsync(request, token.Token);
+            var streamReader = new StreamReader(response.Content.ReadAsStream());
+
+            var result = JsonConvert.DeserializeObject<List<University>>(streamReader.ReadToEnd());
+
+            return result;
+        }
+
+        private CoinDesk GetBitcoin()
+        {
+            Thread.Sleep(1500);
+            var url = BITCOIN_API_URL;
+            var request = new HttpRequestMessage(HttpMethod.Get, url)
+            {
+                Content = new StringContent("", Encoding.UTF8, "application/json")
+            };
+
+            var response = _client.Send(request);
+            var streamReader = new StreamReader(response.Content.ReadAsStream());
+
+            var result = JsonConvert.DeserializeObject<CoinDesk>(streamReader.ReadToEnd());
+
+            return result;
+        }
+
+        #region Multithreading with ThreadPool
+
+        //private void Search_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        BeforeLoadingStockData();
+
+        //        var country = CountryNameInput.Text;
+
+        //        ThreadPool.QueueUserWorkItem((state) =>
+        //        {
+        //            var result = LoadUniversities(country);
+
+        //            Dispatcher.Invoke(() =>
+        //            {
+        //                UniversitiesDataGrid.ItemsSource = null;
+        //                UniversitiesDataGrid.ItemsSource = result;
+        //            });
+        //        });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Notes.Text = ex.Message;
+        //    }
+        //    finally
+        //    {
+        //        AfterLoadingStockData();
+        //    }
+        //}
+
+        //private void Bitcoin_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        ThreadPool.QueueUserWorkItem((state) =>
+        //        {
+        //            var coin = GetBitcoin();
+        //            bitcoins.Add(coin);
+
+        //            Dispatcher.Invoke(() =>
+        //            {
+        //                Coins.ItemsSource = null;
+        //                Coins.ItemsSource = bitcoins;
+        //            });
+        //        });
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        Notes.Text = ex.Message;
+        //    }
+        //    finally
+        //    {
+        //        AfterLoadingStockData();
+        //    }
+        //}
+
+        //private List<University> LoadUniversities(string country)
+        //{
+        //    Thread.Sleep(2500);
+        //    string url = string.Empty;
+
+        //    if (string.IsNullOrEmpty(country))
+        //    {
+        //        url = UNIVERSITIES_API_URL;
+        //    }
+        //    else
+        //    {
+        //        url = UNIVERSITIES_API_URL + "country=" + country;
+        //    }
+
+        //    var request = new HttpRequestMessage(HttpMethod.Get, url)
+        //    {
+        //        Content = new StringContent("", Encoding.UTF8, "application/json")
+        //    };
+
+        //    var response = _client.Send(request);
+        //    var streamReader = new StreamReader(response.Content.ReadAsStream());
+
+        //    var result = JsonConvert.DeserializeObject<List<University>>(streamReader.ReadToEnd());
+
+        //    return result;
+        //}
+
+        //private CoinDesk GetBitcoin()
+        //{
+        //    Thread.Sleep(1500);
+        //    var url = BITCOIN_API_URL;
+        //    var request = new HttpRequestMessage(HttpMethod.Get, url)
+        //    {
+        //        Content = new StringContent("", Encoding.UTF8, "application/json")
+        //    };
+
+        //    var response = _client.Send(request);
+        //    var streamReader = new StreamReader(response.Content.ReadAsStream());
+
+        //    var result = JsonConvert.DeserializeObject<CoinDesk>(streamReader.ReadToEnd());
+
+        //    return result;
+        //}
+
+        #endregion
+
+        private void BeforeLoadingStockData()
+        {
+            stopwatch.Restart();
+            StockProgress.Visibility = Visibility.Visible;
+            StockProgress.IsIndeterminate = true;
+        }
+
+        private void AfterLoadingStockData()
+        {
+            StocksStatus.Text = $"Loaded universities in {stopwatch.ElapsedMilliseconds}ms";
+            StockProgress.Visibility = Visibility.Hidden;
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
