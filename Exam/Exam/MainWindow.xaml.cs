1a614a4b6cf7203b778b71955c1edb8eab4aab00
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-16 21:47:55 +0500

    help
    HELP ME

--- Exam/Exam/MainWindow.xaml.cs
+++ Exam/Exam/MainWindow.xaml.cs
@@ -1,7 +1,14 @@
-﻿using System;
+﻿using Exam.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +19,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
 
 namespace Exam
 {
@@ -20,22 +28,97 @@ namespace Exam
     /// </summary>
     public partial class MainWindow : Window
     {
-        public const string STOCKS_API_URL = "https://ps-async.fekberg.com/api/stocks/";
+        public string STOCKS_API_URL = "https://ps-async.fekberg.com/api/stocks/";
         public const string BITCOIN_API_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+        private List<Coins> list = new List<Coins>();
+        private List<Kompaniya> _list = new List<Kompaniya>();
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Search_Click(object sender, RoutedEventArgs e)
+        private async void Search_Click(object sender, RoutedEventArgs e)
         {
-            // StocksDataGrid.ItemsSource = 
-        }
+           
+            //CountryNameInput
+            var url = STOCKS_API_URL;
 
-        private void Bitcoin_Click(object sender, RoutedEventArgs e)
+            if (string.IsNullOrEmpty(CountryNameInput.Text))
+            {
+                url = STOCKS_API_URL;
+            }
+            else
+            {
+                url = STOCKS_API_URL + "country=" + CountryNameInput.Text;
+            }
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var request = await client.GetAsync(" ");
+            var json = await request.Content.ReadAsStringAsync();
+            var deJson = JsonConvert.DeserializeObject<Kompaniya>(json);
+
+            //faylga yozish Kompanya
+            await DataKompanya(deJson);
+
+            StocksDataGrid.ItemsSource = new List<Kompaniya>();
+            _list.Add(deJson);
+
+            StocksDataGrid.ItemsSource = _list;
+
+        }
+        public async Task DataKompanya(Kompaniya data)
         {
-            // BitcoinsList.ItemsSource = 
+            string path = @"C:\Users\shohb\OneDrive\Ishchi stol\modul-04\Module-04\Exam\Exam\data\data.json";
+            if (!System.IO.File.Exists(path))
+            {
+                System.IO.File.Create(path).Close();
+            }
+            using(StreamWriter  sw = new StreamWriter(path))
+            {
+                await sw.WriteLineAsync(data.change.ToString());
+            }
+        }
+        private async void Bitcoin_Click(object sender, RoutedEventArgs e)
+        {
+            BitcoinsList.ItemsSource = new List<Coins>();
+            var coinDesk = await RequestBitCoinsAsync();
+           
+            list.Add(coinDesk);
+
+            BitcoinsList.ItemsSource = list;
+        }
+        public async Task<Coins> RequestBitCoinsAsync()
+        {
+            var url = BITCOIN_API_URL;
+            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var request = await client.GetAsync("");
+            var Json = await request.Content.ReadAsStringAsync();
+
+            // faylga yozish bitcoins
+            string path = @"C:\Users\shohb\OneDrive\Ishchi stol\modul-04\Module-04\Exam\Exam\data\coin.txt";
+            if (!System.IO.File.Exists(path))
+            {
+                System.IO.File.Create(path).Close();
+            }
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine(Json.ToString());
+            }
+
+
+            var Deserializer = JsonConvert.DeserializeObject<Coins>(Json);
+            return Deserializer;
+        }
+       
+
+        private void CountryNameInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Exam/Exam/Models/Kompaniya.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class Kompaniya
+    {
+        public string ticker { get; set; }
+        public string identifier { get; set; }
+        public DateTime tradeDate { get; set; }
+        public object open { get; set; }
+        public object high { get; set; }
+        public object low { get; set; }
+        public object close { get; set; }
+        public int volume { get; set; }
+        public double change { get; set; }
+        public double changePercent { get; set; }
+
+        public override string ToString()
+        {
+            return $"DateTime:{tradeDate}, Ticker:{ticker}, ChangePercent:{changePercent}";
+        }
+    }
+
+
+}
